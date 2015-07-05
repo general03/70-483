5c95f81e3297e989d906717887a484f1b1d6ce0c
RIGAUDIE David <f322p3ph@yahoo.fr>
2015-07-05 14:20:46 +0200

    Using foreground thread

---
+++ background/README.md
@@ -0,0 +1,9 @@
+﻿#Background VS Foreground thread
+
+Foreground threads are used when you want the thread continue after the program ended
+
+Background threads are finished after the program stop
+
+#Example
+
+In this example if you set IsBackground to false (so foreground thread) the thread continue and display the informations
---
+++ background/ThreadBackground.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace background
+{
+    class ThreadBackground
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Thread : {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void Main()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            // The program stop immediately ...
+            t.IsBackground = true;
+            //... after this line
+            t.Start(); 
+        }
+        
+    }
+}
