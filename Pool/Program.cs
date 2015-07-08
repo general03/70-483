3af675b96763bafa637da07a932bf49ebfb388fd
RIGAUDIE David <f322p3ph@yahoo.fr>
2015-07-08 22:07:59 +0200

    Add thread pool

---
+++ Pool/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Pool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                Console.WriteLine("A thread from thread pool");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pool/README.md
@@ -0,0 +1,5 @@
+﻿# Why thread pool
+
+For reuse a thread when one finished. Indeed the creating thread can cost in time and resource
+
+The pool manages all the threads inside.
