6135860ed3bf0d49976b60be55c6ac636a5220a0
RIGAUDIE David <f322p3ph@yahoo.fr>
2015-07-15 22:16:28 +0200

    Using Parallel class

--- Parallel.cs/Properties/AssemblyInfo.cs
+++ Parallel.cs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Parallel.cs")]
+[assembly: AssemblyTitle("ParallelClass")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Parallel.cs")]
+[assembly: AssemblyProduct("ParallelParallelClass")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
