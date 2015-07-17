c4532c4516b1eb628826cf6a058ce1f6aad8624b
RIGAUDIE David <f322p3ph@yahoo.fr>
2015-07-17 20:33:37 +0200

    Using async and await from C#5

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(DownloadContent().Result);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Asynchronous method with async keyword
+        /// </summary>
+        public static async Task<string> DownloadContent()
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                // Wait the end of this asynchronous method to return the result
+                return await client.GetStringAsync("http://www.microsoft.com");
+            }
+        }
+    }
+}
